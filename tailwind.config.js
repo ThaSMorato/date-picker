668fff193131beb152ae3243a0626125803061a6
ThaSMorato <tha.s.morato@gmail.com>
2024-04-22 21:13:12 -0300

    Chore: initial configs

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          DEFAULT: '#858585',
+          dark: {
+            DEFAULT: '#121212',
+            muted: '#2F2F2F',
+          },
+          base: '#A0A0A0',
+          light: '#F9F7F7',
+        },
+        red: {
+          primary: '#F36A6A',
+          secondary: '#FF7070',
+        },
+        green: '#62D0A2',
+        blue: {
+          primary: '#5865F2',
+          // secondary: '',
+          faded: {
+            primary: '#F1F2FF',
+            // secondary: '',
+          },
+          dark: '#131FA3',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
